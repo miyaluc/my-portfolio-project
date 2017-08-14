c2c13f0cb74b97fc183f9ce81fa844443b99866b
Miya Lucas <srlucas3@gmail.com>
2017-08-13 19:21:32 -0700

    readying for resubmit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function aboutMe () {
+var insights = [];
+
+function AboutMe () {
   //here is my empty constructor function - I'm not sure what I should include in it at this time
   this.name = 'Miya Lucas';
   this.location = 'Seattle, WA';
