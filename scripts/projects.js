76bbe2c26092d09e6859a8a6c1449c0f58a264cf
miyaluc <srlucas3@gmail.com>
2017-08-24 21:01:02 -0700

    added new js folder and separated constructor from data

---
+++ scripts/projects.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var rawData = [
+  {
+    name: 'We Don\'t Know Jack',
+    about: 'A Game that Tests Your Knowledge of Everyday Trivia',
+    image: '',
+  },
+  {
+    name: 'Bus Mall',
+    about: 'Your Online Bus Retailer',
+    image: '',
+  },
+  {
+    name: 'Chocolate Pizza',
+    about: 'A Chocolate Pizza Recipe',
+    image: '',
+  },
+]
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,20 @@
 'use strict';
 var portfolio = [];
 
+//here is my project constructor
 function PastProjects(name, about, screenshot) {
   ///call it = new PastProject(name, about, screenshot)
-  this.name = name;
-  this.about = about;
-  this.screenShot = screenshot;
+  this.name = rawData.name;
+  this.about = rawData.about;
+  this.image = rawData.image;
 }
 
-var youDontKnowJack = new PastProjects('You Don\t Know Jack', 'A Game that Tests Your Knowledge of Everyday Trivia', '');
-var busMall = new PastProjects('Bus Mall', 'Your Online Bus Retailer', '');
-var chocolatePizza = new PastProjects('Chocolate Pizza', 'A Chocolate Pizza Recipe', '');
+//adding all past projects to the DOM
+PastProjects.prototype.displayProject = function() {
+  var $pastProjects = $('section.template').clone();
+  $pastProjects.find('h2').text(this.name);
+  $pastProjects.find('img').attr('src', this.image);
+  $pastProjects.find('p.project-description').text(this.about);
+  $pastProjects.removeClass('template');
+  return $pastProjects;
+}
